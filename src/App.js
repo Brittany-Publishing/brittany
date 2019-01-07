2b9a350428db852bb71709174c7562dd77af4d78
Luke Rebarchik <lrebarchik@farmjournal.com>
2019-01-07 15:36:11 -0600

    fj laptop changes.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './img/prof.jpg';
+import './css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Brittany Rebarchik</h1>
+          <div className="splash"><img src={logo} className="App-logo" alt="logo" /></div>
+          <p>Mauris congue, rem platea rhoncus dolores facilis fusce ullamco rem venenatis, impedit. Curae pulvinar dolores culpa adipisci, cillum? Euismod, quisque tellus urna recusandae nec eos mollitia? Gravida ultrices temporibus eos, aenean erat facilis consectetuer! Nullam! Repudiandae, eget doloremque placeat delectus sit inventore consectetur ridiculus in laboriosam vestibulum qui perspiciatis facilisis! Cupidatat voluptas totam tempor integer, sem officia vero debitis tristique. Curabitur elit sapien ante facilis incidunt! Conubia nesciunt dolores ullam, ullamco ullamco. Aenean vulputate occaecat quaerat, dictumst aliquam recusandae class, incidunt augue luctus expedita nulla voluptatum sociosqu et mollis eiusmod, dignissimos morbi. Itaque soluta aliquet autem, maiores, sollicitudin leo erat.</p>
+          <p>Dignissim condimentum suspendisse tincidunt! Facere hendrerit beatae! Modi, nisi nunc recusandae suscipit cupidatat magna congue tincidunt accusantium conubia? Vehicula voluptatem, consequatur, venenatis habitasse habitasse cum habitant, porta sociosqu! Voluptate, sit incidunt tempora ratione torquent magnis arcu laboris illum, non? Class rhoncus augue alias augue maecenas. Consequat nostrud tenetur, autem ullam molestias cras quos do! Dolores officiis, sapiente modi nostrud bibendum! Adipiscing hic ultrices, reiciendis? Porttitor, fugiat in eligendi eum scelerisque, nulla fames non suscipit excepteur habitasse proident adipisci feugiat volutpat praesentium cupidatat eleifend. Condimentum! Dolore, laoreet diam? Euismod, commodo commodi? Ipsa pulvinar, fermentum fuga facilisi suspendisse, hac lectus tempore porro.</p>
+          <p>Volutpat molestie nam aliqua fugiat? Dictum, quod posuere? Eligendi! Aliquid ultricies blandit explicabo! Quibusdam sociis? Nesciunt id cupidatat augue! Voluptas pharetra, veritatis cupidatat nostra! Lorem, iure pede nostra lacus recusandae saepe incididunt, nonummy, urna! Enim, augue facilisi assumenda aptent, pulvinar? Eget iaculis sit odit nobis aperiam augue sequi eum deserunt? Do temporibus corporis suscipit, scelerisque voluptatibus, conubia arcu sint odit, expedita irure posuere doloremque rhoncus doloremque hac natoque sint quae potenti ullam. Aute autem? Do, sit, suscipit, eius iusto facilisi. Nihil consectetur quaerat dictum fugiat scelerisque mollit cras consectetur non, totam tincidunt numquam litora laoreet litora viverra impedit scelerisque eleifend.</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
